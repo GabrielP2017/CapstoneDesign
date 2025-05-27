27beb603e97cd45b5b022dd70b8401ffd8152dc1
Rupang <dnflajt3@naver.com>
2025-05-27 20:36:47 +0900

    s

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -15,6 +15,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://43.203.44.237:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
