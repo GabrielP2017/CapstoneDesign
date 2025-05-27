17eb4010ff8461576ab29fc19649dee6965a6257
Rupang <dnflajt3@naver.com>
2025-05-27 22:20:18 +0900

    Revert "22"
    This reverts commit b8089fb767dcdb13932a32231856c71248a7459e.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,15 +8,3 @@ export default defineConfig({
   sever: { host: "0.0.0.0" },
   base: "./",
 });
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  sever: { host: "0.0.0.0" },
-  base: "./",
-});
->>>>>>> 5383d10 (최신)
