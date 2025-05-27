4839b6d4d7eb60094204598478958a278b4f7c6b
Rupang <dnflajt3@naver.com>
2025-05-27 19:39:32 +0900

    s

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  sever: {host: "0.0.0.0"}
-})
+export default ({ mode }) => {
+  // mode는 'development' 또는 'production' 등
+  const env = loadEnv(mode, process.cwd(), "");
 
+  return defineConfig({
+    define: {
+      __APP_ENV__: env.APP_ENV, // 선택적
+    },
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 5173,
+      host: "0.0.0.0",
+    },
+  });
+};
